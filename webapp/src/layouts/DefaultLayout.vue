393d7c1f7dfee9815c2f32135de400fc20289a21
JAWORRRRRR <baki.jawor@gmail.com>
2025-11-23 17:19:25 +0100

    Button for Theme change

--- webapp/src/layouts/DefaultLayout.vue
+++ webapp/src/layouts/DefaultLayout.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import { ref } from 'vue';
+import { useTheme } from 'vuetify';
 
 const placeholderLinks = ref([
   { title: "Panel glowny", icon: "mdi-view-dashboard-outline" },
@@ -9,13 +10,15 @@ const placeholderLinks = ref([
 ]);
 
 const drawer = ref(true);
+const theme = useTheme();
+
 </script>
 
 <template>
   <v-app>
     <v-app-bar color="primary" prominent>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>eFaktur</v-toolbar-title>
+      <v-toolbar-title>System e-Faktur</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn icon>
         <v-icon>mdi-account-circle</v-icon>
@@ -33,6 +36,10 @@ const drawer = ref(true);
           link
         ></v-list-item>
       </v-list>
+      <v-btn @click="theme.toggle(['light', 'dark'])" icon>
+        <v-icon v-if="theme.global.name.value == 'light'">mdi-weather-night</v-icon>
+        <v-icon v-else>mdi-weather-sunny</v-icon>
+      </v-btn>
     </v-navigation-drawer>
 
     <v-main>
@@ -42,3 +49,20 @@ const drawer = ref(true);
     </v-main>
   </v-app>
 </template>
+
+<style scoped>
+  .v-list-item {
+    cursor: pointer;
+    border: 2px solid #EEEEEE;
+  }
+
+  .v-navigation-drawer {
+    border-right: 2px solid #EEEEEE;
+  }
+
+  .v-navigation-drawer .v-btn {
+    position: absolute;
+    bottom: 16px;
+    right: 16px;
+  }
+</style>
