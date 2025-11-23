7414fb131cca7431d9ad66ec1a9324514ebfee1a
JAWORRRRRR <baki.jawor@gmail.com>
2025-11-23 16:15:13 +0100

    New Layout + Main view

--- webapp/src/layouts/DefaultLayout.vue
+++ webapp/src/layouts/DefaultLayout.vue
@@ -1,90 +1,44 @@
 <script setup lang="ts">
-const placeholderLinks = [
+import { RouterView } from 'vue-router';
+import { ref } from 'vue';
+
+const placeholderLinks = ref([
   { title: "Panel glowny", icon: "mdi-view-dashboard-outline" },
   { title: "Nowa zakladka", icon: "mdi-bookmark-outline" },
   { title: "Miejsce na modul", icon: "mdi-dots-horizontal" },
-];
+]);
+
+const drawer = ref(true);
 </script>
 
 <template>
   <v-app>
-    <v-app-bar color="primary" dark class="full-width-app-bar">
-      <v-toolbar-title>System eFaktur</v-toolbar-title>
-      <v-spacer />
-      <v-btn color="white" variant="outlined" :to="{ name: 'login' }">
-        Zaloguj się
+    <v-app-bar color="primary" prominent>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>eFaktur</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-account-circle</v-icon>
       </v-btn>
     </v-app-bar>
 
-    <div class="app-layout">
-      <v-navigation-drawer
-        permanent
-        elevation="1"
-        width="260"
-        class="sidebar"
-      >
-        <v-list density="comfortable">
-          <v-divider class="mb-2" />
-          <v-list-item
-            v-for="item in placeholderLinks"
-            :key="item.title"
-            disabled
-          >
-            <template #prepend>
-              <v-icon :icon="item.icon" />
-            </template>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+    <v-navigation-drawer v-model="drawer" permanent>
+      <v-list nav density="compact">
+        <v-list-item 
+          v-for="link in placeholderLinks"
+          :key="link.title"
+          :prepend-icon="link.icon" 
+          :title="link.title" 
+          :value="link.title"
+          link
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-      <v-main class="bg-grey-lighten-4 content-area">
+    <v-main>
+      <v-container fluid>
         <RouterView />
-      </v-main>
-    </div>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
-
-<style scoped>
-.full-width-app-bar {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  z-index: 10;
-}
-
-.app-layout {
-  display: flex;
-  min-height: 100vh;
-  margin-top: 64px;
-}
-
-.sidebar {
-  height: calc(100vh - 64px);
-}
-
-.content-area {
-  flex: 1;
-  padding: 32px 16px;
-}
-
-@media (max-width: 600px) {
-  .app-layout {
-    flex-direction: column;
-    margin-top: 56px;
-  }
-
-  .sidebar {
-    height: auto;
-    width: 100%;
-  }
-}
-</style>
-
-<style scoped>
-.full-width-app-bar {
-  left: 0 !important;
-  width: 100% !important;
-}
-</style>
--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import "vuetify/styles/main.sass";
 import { createVuetify } from "vuetify";
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import App from "./App.vue";
@@ -12,7 +14,14 @@ const vuetify = createVuetify({
   components,
   directives,
   theme: {
-    defaultTheme: "dark",
+    defaultTheme: "light",
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
   },
 });
 
