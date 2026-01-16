49e7c0dce4d6307a7a070005fa4ff01cdddeec25
venoxdevpl <kamil@venoxdev.pl>
2026-01-16 02:18:01 +0100

    StorageService

--- apps/api/src/actions/me/setCompanyData.ts
+++ apps/api/src/actions/me/setCompanyData.ts
@@ -1,6 +1,7 @@
 import { RouteCtx, contract } from "@repo/contract";
 import { useRepository } from "../../core/database";
 import { Company } from "../../models/Company";
+import { StorageService } from "../../services/StorageService";
 
 export type Route = RouteCtx<typeof contract.setCompanyData>;
 
@@ -16,7 +17,8 @@ export const setCompanyData = async (ctx: Route["ctx"]): Promise<Route["response
     if (company) {
         companyRepository.merge(company, ctx.body);
     } else {
-        await companyRepository.save(companyRepository.create(ctx.body));
+        const { id } = await companyRepository.save(companyRepository.create(ctx.body));
+        StorageService.initForCompany(id);
     }
 
     return {
